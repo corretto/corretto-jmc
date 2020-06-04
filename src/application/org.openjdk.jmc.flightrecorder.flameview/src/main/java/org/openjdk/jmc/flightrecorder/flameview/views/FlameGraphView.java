6122405e856e46dae33f25ee3749db8ed0e7a8fa
Ziyi Luo <luoziyi@amazon.com>
2020-06-04 17:30:55 +0000

    7.1.1-ga: Import from upstream

---
+++ src/application/org.openjdk.jmc.flightrecorder.flameview/src/main/java/org/openjdk/jmc/flightrecorder/flameview/views/FlameGraphView.java
@@ -0,0 +1,283 @@
+/*
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, Datadog, Inc. All rights reserved.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The contents of this file are subject to the terms of either the Universal Permissive License
+ * v 1.0 as shown at http://oss.oracle.com/licenses/upl
+ *
+ * or the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided with
+ * the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.openjdk.jmc.flightrecorder.flameview.views;
+
+import java.io.IOException;
+
+import java.text.MessageFormat;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.ProgressAdapter;
+import org.eclipse.swt.browser.ProgressEvent;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IMemento;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.ViewPart;
+import org.openjdk.jmc.common.item.IItemCollection;
+import org.openjdk.jmc.common.util.StringToolkit;
+import org.openjdk.jmc.flightrecorder.flameview.tree.TraceNode;
+import org.openjdk.jmc.flightrecorder.flameview.tree.TraceTreeUtils;
+import org.openjdk.jmc.flightrecorder.stacktrace.FrameSeparator;
+import org.openjdk.jmc.flightrecorder.stacktrace.FrameSeparator.FrameCategorization;
+import org.openjdk.jmc.flightrecorder.ui.FlightRecorderUI;
+import org.openjdk.jmc.flightrecorder.ui.messages.internal.Messages;
+import org.openjdk.jmc.ui.CoreImages;
+import org.openjdk.jmc.ui.common.util.AdapterUtil;
+import org.openjdk.jmc.ui.handlers.MCContextMenuManager;
+import org.openjdk.jmc.ui.misc.DisplayToolkit;
+
+public class FlameGraphView extends ViewPart implements ISelectionListener {
+	private static final String HTML_PAGE;
+	static {
+		// from: https://cdn.jsdelivr.net/gh/spiermar/d3-flame-graph@2.0.3/dist/d3-flamegraph.css
+		String cssD3Flamegraph = "jslibs/d3-flamegraph.css";
+		// from: https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js
+		String jsHtml5shiv = "jslibs/html5shiv.min.js";
+		// from: https://oss.maxcdn.com/respond/1.4.2/respond.min.js
+		String jsRespond = "jslibs/respond.min.js";
+		// from: https://d3js.org/d3.v4.min.js
+		String jsD3V4 = "jslibs/d3.v4.min.js";
+		// from: https://cdnjs.cloudflare.com/ajax/libs/d3-tip/0.9.1/d3-tip.min.js
+		String jsD3Tip = "jslibs/d3-tip.min.js";
+		// from: https://cdn.jsdelivr.net/gh/spiermar/d3-flame-graph@2.0.3/dist/d3-flamegraph.min.js
+		String jsD3FlameGraph = "jslibs/d3-flamegraph.min.js";
+
+		String jsIeLibraries = loadLibraries(jsHtml5shiv, jsRespond);
+		String jsD3Libraries = loadLibraries(jsD3V4, jsD3Tip, jsD3FlameGraph);
+
+		// formatter arguments for the template: %1 - CSSs, %2 - IE9 specific scripts, %3 - 3rd party scripts
+		HTML_PAGE = String.format(fileContent("page.template"), 
+				fileContent(cssD3Flamegraph),
+				jsIeLibraries,
+				jsD3Libraries);
+	}
+
+	private static final ExecutorService MODEL_EXECUTOR = Executors.newFixedThreadPool(1);
+	private FrameSeparator frameSeparator;
+
+	private Browser browser;
+	private SashForm container;
+	private TraceNode currentRoot;
+	private CompletableFuture<TraceNode> currentModelCalculator;
+	private boolean threadRootAtTop = true;
+	private IItemCollection currentItems;
+	private GroupByAction[] groupByActions;
+
+	private class GroupByAction extends Action {
+
+		private final boolean fromThreadRootAction;
+
+		GroupByAction(boolean fromRoot) {
+			super(fromRoot ? Messages.STACKTRACE_VIEW_THREAD_ROOT : Messages.STACKTRACE_VIEW_LAST_FRAME,
+					IAction.AS_RADIO_BUTTON);
+			fromThreadRootAction = fromRoot;
+			setToolTipText(fromRoot ? Messages.STACKTRACE_VIEW_GROUP_TRACES_FROM_ROOT
+					: Messages.STACKTRACE_VIEW_GROUP_TRACES_FROM_LAST_FRAME);
+			setImageDescriptor(fromRoot ? CoreImages.THREAD : CoreImages.METHOD_NON_OPTIMIZED);
+			setChecked(fromRoot == threadRootAtTop);
+		}
+
+		@Override
+		public void run() {
+			boolean newValue = isChecked() == fromThreadRootAction;
+			if (newValue != threadRootAtTop) {
+				threadRootAtTop = newValue;
+				rebuildModel(currentItems);
+			}
+		}
+	}
+
+	@Override
+	public void init(IViewSite site, IMemento memento) throws PartInitException {
+		super.init(site, memento);
+		frameSeparator = new FrameSeparator(FrameCategorization.METHOD, false);
+		groupByActions = new GroupByAction[] {new GroupByAction(false), new GroupByAction(true)};
+
+		//methodFormatter = new MethodFormatter(null, () -> viewer.refresh());
+		IMenuManager siteMenu = site.getActionBars().getMenuManager();
+		siteMenu.add(new Separator(MCContextMenuManager.GROUP_TOP));
+		siteMenu.add(new Separator(MCContextMenuManager.GROUP_VIEWER_SETUP));
+		// addOptions(siteMenu);
+		IToolBarManager toolBar = site.getActionBars().getToolBarManager();
+		Stream.of(groupByActions).forEach(toolBar::add);
+		getSite().getPage().addSelectionListener(this);
+	}
+
+	@Override
+	public void dispose() {
+		getSite().getPage().removeSelectionListener(this);
+		super.dispose();
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		container = new SashForm(parent, SWT.HORIZONTAL);
+		browser = new Browser(container, SWT.NONE);
+		container.setMaximizedControl(browser);
+	}
+
+	@Override
+	public void setFocus() {
+		browser.setFocus();
+	}
+
+	@Override
+	public void saveState(IMemento memento) {
+	}
+
+	@Override
+	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			Object first = ((IStructuredSelection) selection).getFirstElement();
+			setItems(AdapterUtil.getAdapter(first, IItemCollection.class));
+		}
+	}
+
+	private void setItems(IItemCollection items) {
+		if (items != null) {
+			currentItems = items;
+			rebuildModel(items);
+		}
+	}
+
+	private void rebuildModel(IItemCollection items) {
+		// Release old model before building the new
+		if (currentModelCalculator != null) {
+			currentModelCalculator.cancel(true);
+		}
+		currentModelCalculator = getModelPreparer(items, frameSeparator, true);
+		currentModelCalculator.thenAcceptAsync(this::setModel, DisplayToolkit.inDisplayThread())
+				.exceptionally(FlameGraphView::handleModelBuildException);
+	}
+
+	private CompletableFuture<TraceNode> getModelPreparer(
+		final IItemCollection items, final FrameSeparator separator, final boolean materializeSelectedBranches) {
+		return CompletableFuture.supplyAsync(() -> {
+			return TraceTreeUtils.createTree(items, separator, threadRootAtTop, "-- <Root> --");
+		}, MODEL_EXECUTOR);
+	}
+
+	private void setModel(TraceNode root) {
+		if (!browser.isDisposed() && !root.equals(currentRoot)) {
+			currentRoot = root;
+			setViewerInput(root);
+		}
+	}
+
+	private void setViewerInput(TraceNode root) {
+		browser.setText(HTML_PAGE);
+		browser.addProgressListener(new ProgressAdapter() {
+			@Override
+			public void completed(ProgressEvent event) {
+				browser.removeProgressListener(this);
+				browser.execute(String.format("processGraph(%s);", toJSon(root)));
+			}
+		});
+	}
+
+	private static Void handleModelBuildException(Throwable ex) {
+		if (!(ex.getCause() instanceof CancellationException)) {
+			FlightRecorderUI.getDefault().getLogger().log(Level.SEVERE, "Failed to build stacktrace view model", ex); //$NON-NLS-1$
+		}
+		return null;
+	}
+
+	private static String toJSon(TraceNode root) {
+		if (root == null) {
+			return "\"\"";
+		}
+		return render(root);
+	}
+
+	private static String render(TraceNode root) {
+		StringBuilder builder = new StringBuilder();
+		render(builder, root);
+		return builder.toString();
+	}
+
+	private static void render(StringBuilder builder, TraceNode node) {
+		String start = String.format("{%s,%s, \"c\": [ ", toJSonKeyValue("n", node.getName()),
+				toJSonKeyValue("v", String.valueOf(node.getValue())));
+		builder.append(start);
+		for (int i = 0; i < node.getChildren().size(); i++) {
+			render(builder, node.getChildren().get(i));
+			if (i < node.getChildren().size() - 1) {
+				builder.append(",");
+			}
+		}
+		builder.append("]}");
+	}
+
+	private static String toJSonKeyValue(String key, String value) {
+		return "\"" + key + "\": " + "\"" + value + "\"";
+	}
+
+	private static String loadLibraries(String... libs) {
+		if(libs == null || libs.length == 0) {
+			return "";
+		} else {
+			return Stream.of(libs).map(FlameGraphView::fileContent).collect(Collectors.joining("\n"));
+		}
+	}
+
+	private static String fileContent(String fileName){
+		try {
+			return StringToolkit.readString(FlameGraphView.class.getClassLoader().getResourceAsStream(fileName));
+		} catch (IOException e) {
+			FlightRecorderUI.getDefault().getLogger()
+				.log(Level.WARNING, MessageFormat
+						.format("Could not load script \"{0}\",\"{1}\"", fileName, e.getMessage()));  	//$NON-NLS-1$
+			return "";
+		}
+	}
+}
---
+++ src/core/org.openjdk.jmc.agent/src/test/java/org/openjdk/jmc/agent/test/InstrumentMe.java
@@ -0,0 +1,323 @@
+/*
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * 
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The contents of this file are subject to the terms of either the Universal Permissive License
+ * v 1.0 as shown at http://oss.oracle.com/licenses/upl
+ *
+ * or the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided with
+ * the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.openjdk.jmc.agent.test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.openjdk.jmc.agent.test.util.TestToolkit;
+
+public class InstrumentMe {
+	public static void main(String[] args) throws InterruptedException, IOException {
+		Thread runner = new Thread(new Runner(), "InstrumentMe Runner");
+		runner.setDaemon(true);
+		System.out.println("Press <enter> at any time to quit");
+		System.out.println("Now starting looping through the instrumentation examples");
+		runner.start();
+		System.in.read();
+	}
+
+	private final static class Runner implements Runnable {
+		public void run() {
+			InstrumentMe instance = new InstrumentMe();
+			while (true) {
+				try {
+					InstrumentMe.runStatic();
+					InstrumentMe.runInstance(instance);
+				} catch (InterruptedException e) {
+				}
+			}
+		}
+	}
+
+	private static void runInstance(InstrumentMe instance) throws InterruptedException {
+		System.out.println("Running instance versions..."); //$NON-NLS-1$
+		instance.printInstanceHelloWorld1();
+		instance.printInstanceHelloWorld2(TestToolkit.randomString(), TestToolkit.randomLong());
+		instance.printInstanceHelloWorld3(Gurka.createGurka());
+		instance.printInstanceHelloWorld4(new Gurka[] {Gurka.createGurka(), Gurka.createGurka()});
+		instance.printInstanceHelloWorld5(createGurkList());
+		instance.printInstanceHelloWorldJFR1();
+		instance.printInstanceHelloWorldJFR2(TestToolkit.randomString(), TestToolkit.randomLong());
+		instance.printInstanceHelloWorldJFR3(Gurka.createGurka());
+		instance.printInstanceHelloWorldJFR4(new Gurka[] {Gurka.createGurka(), Gurka.createGurka()});
+		instance.printInstanceHelloWorldJFR5(createGurkList());
+		instance.printInstanceHelloWorldJFR6();
+		instance.printInstanceHelloWorldJFR7();
+		try {
+			instance.printInstanceHelloWorldJFR8();
+		} catch (RuntimeException e) {
+			System.out.println("#IJFR8. Caught a RuntimeException: " + e.getMessage());
+		}
+		try {
+			instance.printInstanceHelloWorldJFR9();
+		} catch (RuntimeException e) {
+			System.out.println("#IJFR9. Caught a RuntimeException: " + e.getMessage());
+		}
+		try {
+			instance.printInstanceHelloWorldJFR10();
+		} catch (RuntimeException e) {
+			System.out.println("#IJFR10. Caught a RuntimeException: " + e.getMessage());
+		}
+	}
+
+	private static void runStatic() throws InterruptedException {
+		System.out.println("Running static versions..."); //$NON-NLS-1$
+		printHelloWorld1();
+		printHelloWorld2(TestToolkit.randomString(), TestToolkit.randomLong());
+		printHelloWorld3(Gurka.createGurka());
+		printHelloWorld4(new Gurka[] {Gurka.createGurka(), Gurka.createGurka()});
+		printHelloWorld5(createGurkList());
+		printHelloWorldJFR1();
+		printHelloWorldJFR2(TestToolkit.randomString(), TestToolkit.randomLong());
+		printHelloWorldJFR3(Gurka.createGurka());
+		printHelloWorldJFR4(new Gurka[] {Gurka.createGurka(), Gurka.createGurka()});
+		printHelloWorldJFR5(createGurkList());
+		printHelloWorldJFR6();
+		printHelloWorldJFR7();
+		try {
+			printHelloWorldJFR8();
+		} catch (RuntimeException e) {
+			System.out.println("#IJFR8. Caught a RuntimeException: " + e.getMessage());
+		}
+		try {
+			printHelloWorldJFR9();
+		} catch (RuntimeException e) {
+			System.out.println("#IJFR9. Caught a RuntimeException: " + e.getMessage());
+		}
+		try {
+			printHelloWorldJFR10();
+		} catch (RuntimeException e) {
+			System.out.println("#IJFR10. Caught a RuntimeException: " + e.getMessage());
+		}
+	}
+
+	private static Collection<Gurka> createGurkList() {
+		List<Gurka> gurkList = new ArrayList<>();
+		for (int i = 0; i < TestToolkit.RND.nextInt(4) + 1; i++) {
+			gurkList.add(Gurka.createGurka());
+		}
+		return gurkList;
+	}
+
+	public static void printHelloWorld1() throws InterruptedException {
+		System.out.println("#S1. Hello World!"); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public static int printHelloWorld2(String str, long l) throws InterruptedException {
+		int returnval = TestToolkit.RND.nextInt(45);
+		System.out.println(String.format("#S2. Str:%s long:%d retval:%d", str, l, returnval)); //$NON-NLS-1$
+		Thread.sleep(1000);
+		return returnval;
+	}
+
+	public static void printHelloWorld3(Gurka gurka) throws InterruptedException {
+		System.out.println(String.format("#S3. Got a gurka with id: %d", gurka.getID())); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public static void printHelloWorld4(Gurka[] gurkor) throws InterruptedException {
+		System.out.println(String.format("#S4. Got gurkor: %s", Arrays.toString(gurkor))); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public static void printHelloWorld5(Collection<Gurka> gurkor) throws InterruptedException {
+		System.out.println(String.format("#S5. Got gurkor: %s", String.valueOf(gurkor))); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public static void printHelloWorldJFR1() throws InterruptedException {
+		System.out.println("#SJFR1. Hello World!"); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public static int printHelloWorldJFR2(String str, long l) throws InterruptedException {
+		int returnval = TestToolkit.RND.nextInt(45);
+		System.out.println(String.format("#SJFR2. Str:%s long:%d retval:%d", str, l, returnval)); //$NON-NLS-1$
+		Thread.sleep(1000);
+		return returnval;
+	}
+
+	public static void printHelloWorldJFR3(Gurka gurka) throws InterruptedException {
+		System.out.println(String.format("#SJFR3. Got a gurka with id: %d", gurka.getID())); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public static void printHelloWorldJFR4(Gurka[] gurkor) throws InterruptedException {
+		System.out.println(String.format("#SJFR4. Got gurkor: %s", Arrays.toString(gurkor))); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public static void printHelloWorldJFR5(Collection<Gurka> gurkor) throws InterruptedException {
+		System.out.println(String.format("#SJFR5. Got gurkor: %s", String.valueOf(gurkor))); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public static double printHelloWorldJFR6() throws InterruptedException {
+		double returnval = TestToolkit.RND.nextDouble() * 100;
+		System.out.println(String.format("#SJFR6. retval:%3.3f", returnval)); //$NON-NLS-1$
+		Thread.sleep(1000);
+		return returnval;
+	}
+
+	public static void printHelloWorldJFR7() throws InterruptedException {
+		try {
+			System.out.println("#SJFR7. Hello World!"); //$NON-NLS-1$
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			// intentionally empty
+		}
+	}
+
+	public static void printHelloWorldJFR8() throws InterruptedException {
+		System.out.println("#IJFR8. About to throw a RuntimeException"); //$NON-NLS-1$
+		Thread.sleep(1000);
+		(new ArrayList<>()).get(1);
+	}
+
+	public static void printHelloWorldJFR9() throws InterruptedException {
+		System.out.println("#IJFR9. About to throw a RuntimeException"); //$NON-NLS-1$
+		Thread.sleep(1000);
+		(new ArrayList<>()).get(1);
+	}
+
+	public static void printHelloWorldJFR10() throws InterruptedException {
+		System.out.println("#IJFR10. About to throw a RuntimeException"); //$NON-NLS-1$
+		Thread.sleep(1000);
+
+		try {
+			(new ArrayList<>()).get(1);
+		} catch (RuntimeException e) {
+			System.out.println("#IJFR10. Caught a RuntimeException: " + e.getMessage()); //$NON-NLS-1$
+			throw e;
+		}
+	}
+
+	public void printInstanceHelloWorld1() throws InterruptedException {
+		System.out.println("#I1. Hello World!"); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public int printInstanceHelloWorld2(String str, long l) throws InterruptedException {
+		int returnval = TestToolkit.RND.nextInt(45);
+		System.out.println(String.format("#I2. Str:%s long:%d retval:%d", str, l, returnval)); //$NON-NLS-1$
+		Thread.sleep(1000);
+		return returnval;
+	}
+
+	public void printInstanceHelloWorld3(Gurka gurka) throws InterruptedException {
+		System.out.println(String.format("#I3. Got a gurka with id: %d", gurka.getID())); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public void printInstanceHelloWorld4(Gurka[] gurkor) throws InterruptedException {
+		System.out.println(String.format("#I4. Got gurkor: %s", Arrays.toString(gurkor))); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public void printInstanceHelloWorld5(Collection<Gurka> gurkor) throws InterruptedException {
+		System.out.println(String.format("#I5. Got gurkor: %s", String.valueOf(gurkor))); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public void printInstanceHelloWorldJFR1() throws InterruptedException {
+		System.out.println("#IJFR1. Hello World!"); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public int printInstanceHelloWorldJFR2(String str, long l) throws InterruptedException {
+		int returnval = TestToolkit.RND.nextInt(45);
+		System.out.println(String.format("#IJFR2. Str:%s long:%d retval:%d", str, l, returnval)); //$NON-NLS-1$
+		Thread.sleep(1000);
+		return returnval;
+	}
+
+	public void printInstanceHelloWorldJFR3(Gurka gurka) throws InterruptedException {
+		System.out.println(String.format("#IJFR3. Got a gurka with id: %d", gurka.getID())); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public void printInstanceHelloWorldJFR4(Gurka[] gurkor) throws InterruptedException {
+		System.out.println(String.format("#IJFR4. Got gurkor: %s", Arrays.toString(gurkor))); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public void printInstanceHelloWorldJFR5(Collection<Gurka> gurkor) throws InterruptedException {
+		System.out.println(String.format("#IJFR5. Got gurkor: %s", String.valueOf(gurkor))); //$NON-NLS-1$
+		Thread.sleep(1000);
+	}
+
+	public double printInstanceHelloWorldJFR6() throws InterruptedException {
+		double returnval = TestToolkit.RND.nextDouble();
+		System.out.println(String.format("#IJFR6. retval:%1.3f", returnval)); //$NON-NLS-1$
+		Thread.sleep(1000);
+		return returnval;
+	}
+
+	public void printInstanceHelloWorldJFR7() throws InterruptedException {
+		try {
+			System.out.println("#IJFR7. Hello World!"); //$NON-NLS-1$
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			// intentionally empty
+		}
+	}
+
+	public void printInstanceHelloWorldJFR8() throws InterruptedException {
+		System.out.println("#IJFR8. About to throw a RuntimeException"); //$NON-NLS-1$
+		Thread.sleep(1000);
+		(new ArrayList<>()).get(1);
+	}
+
+	public void printInstanceHelloWorldJFR9() throws InterruptedException {
+		System.out.println("#IJFR9. About to throw a RuntimeException"); //$NON-NLS-1$
+		Thread.sleep(1000);
+		(new ArrayList<>()).get(1);
+	}
+
+	public void printInstanceHelloWorldJFR10() throws InterruptedException {
+		System.out.println("#IJFR10. About to throw a RuntimeException"); //$NON-NLS-1$
+		Thread.sleep(1000);
+
+		try {
+			(new ArrayList<>()).get(1);
+		} catch (RuntimeException e) {
+			System.out.println("#IJFR10. Caught a RuntimeException: " + e.getMessage()); //$NON-NLS-1$
+			throw e;
+		}
+	}
+}
